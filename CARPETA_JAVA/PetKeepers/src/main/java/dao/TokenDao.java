6cf3af2223899f994dc5d6011c4fac86957bd426
Juan Mata <mataguzzojuan@gmail.com>
2022-05-24 19:21:08 +0200

    Creacion de token

--- CARPETA_JAVA/PetKeepers/src/main/java/dao/TokenDao.java
+++ CARPETA_JAVA/PetKeepers/src/main/java/dao/TokenDao.java
@@ -1,11 +1,17 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import config.ConstantsApi;
 import model.Usuario;
@@ -23,10 +29,38 @@ public class TokenDao {
 			bbddConnection.close();
 		}
 	}
-	
-	public String getUsuarios() throws SQLException, ClassNotFoundException {
-		String password = "password";
 
-		return "";
+	public String crearToken(int id) throws SQLException, ClassNotFoundException {
+		// Establecemos la clave secreta
+		String key = "password";
+
+		// Generamos el algotirmo
+		Algorithm alg = Algorithm.HMAC256(key);
+
+		// Generamos el token a partir del id
+		String token = JWT.create().withIssuer("Petkeepers") // EMISOR DE TOKEN
+				.withSubject(String.valueOf(id)) // ID
+				.sign(alg);
+
+		return token;
+	}
+
+	public boolean verificarToken(String token) throws SQLException, ClassNotFoundException {
+
+		// Establecemos la clave secreta
+		String key = "password";
+
+		// Generamos el algotirmo
+		Algorithm alg = Algorithm.HMAC256(key);
+
+		JWTVerifier verifier = JWT.require(alg).withIssuer("Petkeepers").build();
+		try {
+			verifier.verify(token);
+			return true;
+		} catch (JWTVerificationException e) {
+			e.printStackTrace();
+		}
+
+		return false;
 	}
 }
---
+++ CARPETA_JAVA/PetKeepers/src/main/java/service/ServiceToken.java
@@ -0,0 +1,40 @@
+package service;
+
+import java.sql.SQLException;
+import dao.TokenDao;
+
+public class ServiceToken {
+	private TokenDao Dao;
+
+	public String crearToken(int id) throws SQLException, ClassNotFoundException {
+		Dao = new TokenDao();
+		try {
+			Dao.connect();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		String token = Dao.crearToken(id);
+		Dao.disconnect();
+
+		return token;
+	}
+	
+	public Boolean verificarToken(String token) throws SQLException, ClassNotFoundException {
+		Dao = new TokenDao();
+		try {
+			Dao.connect();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		boolean resultado = Dao.verificarToken(token);
+		Dao.disconnect();
+
+		return resultado;
+	}
+}
