b2e1af82d8d7b65357982c694e4e3d66cd49710a
Juan Mata <mataguzzojuan@gmail.com>
2022-05-25 22:10:10 +0200

    arreglos

--- CARPETA_JAVA/PetKeepers/src/main/java/config/ConstantsApi.java
+++ CARPETA_JAVA/PetKeepers/src/main/java/config/ConstantsApi.java
@@ -2,22 +2,23 @@ package config;
 
 public class ConstantsApi {
 	public static final String USER_CONNECTION = "root";
-	/*JUAN:*/
+	/*JUAN:
 	public static final String PASS_CONNECTION = "12341234";
 	public static final String CONNECTION = "jdbc:mysql://localhost:3306/" + "petkeepers"
 			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-
-	/*DIEGO:*/	
+	*/
+	
+	/*DIEGO:	
 	public static final String PASS_CONNECTION = "";
 	public static final String CONNECTION = "jdbc:mysql://localhost:3306/" + "petkeepers"
 			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-	
+	*/
 
-	/*CRISTIAN:*/
+	/*CRISTIAN:
 	public static final String PASS_CONNECTION = "root";
 	public static final String CONNECTION = "jdbc:mysql://localhost:8889/" + "petkeepers"
 			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-	
+	*/
 	
 	// ADQUISISICION DE MASCOTAS Y SERVICIOS
 	public static final String GET_SERVICIOS_BY_CLIENT_ID = "SELECT id_servicio FROM petkeepers.adquirir_servicio WHERE id_cliente = ?";
